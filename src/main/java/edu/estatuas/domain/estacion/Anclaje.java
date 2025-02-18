1dd11cd0f1bafe5fb7368abaa82b8566d5165dfc
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-02-18 13:39:48 +0100

    feat(anclaje): Añadir src del anclaje

---
+++ src/main/java/edu/estatuas/domain/estacion/Anclaje.java
@@ -0,0 +1,33 @@
+package edu.estatuas.domain.estacion;
+
+import edu.estatuas.domain.bicicleta.Movil;
+
+class Anclaje {
+    private boolean ocupado;
+    private Movil bici;
+
+    Anclaje() {
+        this.ocupado = false;
+        this.bici = null;
+    }
+
+    boolean isOcupado() {
+        return ocupado;
+    }
+
+    Movil getBici() {
+        return this.bici;
+    }
+
+    void anclarBici(Movil movil) {
+        this.bici = movil;
+        this.ocupado = true;
+    }
+
+    void liberarBici() {
+        this.bici = null;
+        this.ocupado = false;
+    }
+
+    // TODO: implement toString
+}
