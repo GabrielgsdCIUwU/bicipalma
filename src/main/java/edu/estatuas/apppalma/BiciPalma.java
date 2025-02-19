7ce32e2328089b1c6f067efc086f54c053ef60ff
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-02-19 11:58:44 +0100

    feat(bicipalma): Añadir consultar anclajes y anclar bicicletas

--- src/main/java/edu/estatuas/apppalma/BiciPalma.java
+++ src/main/java/edu/estatuas/apppalma/BiciPalma.java
@@ -2,6 +2,7 @@ package edu.estatuas.apppalma;
 
 import java.util.logging.Logger;
 
+import edu.estatuas.domain.bicicleta.Bicicleta;
 import edu.estatuas.domain.estacion.Estacion;
 
 public class BiciPalma {
@@ -11,11 +12,19 @@ public class BiciPalma {
 
         logger.info("**** caso TEST visualizar estado de la estacion ****");
 
-        Estacion estacion = new Estacion(1, "Manacor", 2);
+        Estacion estacion = new Estacion(1, "Manacor", 6);
         estacion.consultarEstacion();
 
         logger.info("*** caso TEST visualizar anclajes libres ****");
 
         logger.info("anclajes Libres: " + estacion.anclajesLibres());
+
+        logger.info("*** caso TEST anclar bicicletas ****");
+
+        for (int i = 200; i < 204; i++) {
+            estacion.anclarBicicleta(new Bicicleta(i));
+        }
+
+        estacion.consultarAnclajes();
     }
 }
