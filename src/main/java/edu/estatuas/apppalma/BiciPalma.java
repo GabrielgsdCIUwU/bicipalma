15fa868451c01074d02b3a0ada48d1d09ea1e1d2
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-02-19 11:18:29 +0100

    feat(bicipalma): Añadir log de anclajes libres

--- src/main/java/edu/estatuas/apppalma/BiciPalma.java
+++ src/main/java/edu/estatuas/apppalma/BiciPalma.java
@@ -13,5 +13,9 @@ public class BiciPalma {
 
         Estacion estacion = new Estacion(1, "Manacor", 2);
         estacion.consultarEstacion();
+
+        logger.info("*** caso TEST visualizar anclajes libres ****");
+
+        logger.info("anclajes Libres: " + estacion.anclajesLibres());
     }
 }
